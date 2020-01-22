4d447562e2d2683b066eeced6f003d7bb5fc10a1
Paulo Assis <phanet@gmail.com>
2020-01-22 15:38:16 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/phanet/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/phanet/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.phanet.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.phanet.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar(){
-		return "Rest está funcionando!";
+	public List<Categoria> listar(){
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
+		//return "Rest está funcionando!";
 	}
 }
