c3de4923d6361b43df379e01a3335ab75a6a4e1d
Paulo Assis <phanet@gmail.com>
2020-01-28 16:33:52 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/phanet/cursomc/CursomcApplication.java
+++ src/main/java/com/phanet/cursomc/CursomcApplication.java
@@ -1,15 +1,17 @@
 package com.phanet.cursomc;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.phanet.cursomc.domain.Categoria;
+import com.phanet.cursomc.domain.Produto;
 import com.phanet.cursomc.repositories.CategoriaRepository;
+import com.phanet.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -17,6 +19,9 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -27,8 +32,21 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(java.util.Arrays.asList(cat1,cat2));
-
+		produtoRepository.saveAll(java.util.Arrays.asList(p1,p2,p3));
+		
 	}
 
 }
+
